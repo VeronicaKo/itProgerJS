6afb09b59c1b53c6575663d64fc6def48dc91c81
VeronicaKo <VeronicaKo1987@gmail.ru>
2021-09-06 18:46:31 +0300

    Школа itProger Уроки JavaScript

---
+++ js/lesson13.js
@@ -0,0 +1,22 @@
+id = setInterval(my_func,1000);
+
+let counter = 0;
+function my_func() {
+  counter++;
+  console.log(counter);
+  if (counter>2) {
+    clearInterval(id);
+  }
+}
+
+// or
+
+// let counter = 0;
+// setInterval(function() {
+//   counter++;
+//   console.log(counter);
+// },1000);
+
+setTimeout(function () {
+  console.log('Timer is stoped')
+},3000);
---
+++ js/lesson14.js
@@ -0,0 +1,28 @@
+let date=new Date();
+
+console.log(date.getFullYear()+'/'+(date.getMonth()+1) +'/'+date.getDate() + ' '+date.getHours() +':'+date.getMinutes()+':'+date.getSeconds());
+
+let arr=[10,2,8,9,5];
+
+function compareNum(a, b) {
+    return a-b;  }
+arr=arr.sort(compareNum);
+console.log(arr.join("|"));
+console.log(arr.reverse());
+console.log(arr.join(",").split(','));
+
+class Person {
+  constructor(name,age,work) {
+    this.name = name;
+    this.age = age;
+    this.work = work;
+  }
+  info(){
+    console.log('human: '+this.name+' Age: '+this.age+' Job: '+this.work);
+  }
+}
+
+let alex = new Person('alex',45,false);
+let kate = new Person('kate',44,true);
+console.log(alex.age +' in love with ' + kate.age);
+alex.info();
